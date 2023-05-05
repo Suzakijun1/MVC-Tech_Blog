648b37eec00a207e37ea2d840919f66de103b4cb
suzakijun1 <dyong1997@gmail.com>
2023-05-05 15:50:35 -0400

    added userid and post id

--- models/Comment.js
+++ models/Comment.js
@@ -13,6 +13,22 @@ Comment.init(
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
