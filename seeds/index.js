7583e7bc219f4c783f750731ee2ea0f6f74d3ac9
suzakijun1 <dyong1997@gmail.com>
2023-05-05 11:16:20 -0400

    seeds index

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const sequelize = require("../config/connection");
+const { User, Blog, Comment } = require("../models");
+
+const users = [
+  {
+    username: "user1",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    password: "password2",
+  },
+];
+
+const blogs = [
+  {
+    title: "My First Post",
+    content: "This is my first post's contents",
+    userId: 1,
+  },
+];
+
+const comments = [
+  {
+    body: "This is a comment!",
+    blogId: 1,
+    userId: 1,
+  },
+];
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(users, {
+    individualHooks: true,
+  });
+  await Blog.bulkCreate(blogs);
+  await Comment.bulkCreate(comments);
+  process.exit(0);
+};
+seedAll();
