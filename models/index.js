bfa838ff5771a34bf7fe3e3503e7e11731bb7635
suzakijun1 <dyong1997@gmail.com>
2023-05-05 16:03:52 -0400

    model index

--- models/index.js
+++ models/index.js
@@ -2,14 +2,31 @@ const User = require("./User");
 const Blog = require("./Blog");
 const Comment = require("./Comment");
 
-User.hasMany(Blog);
-Blog.belongsTo(User);
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+});
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
 
-Blog.hasMany(Comment);
-Comment.belongsTo(Blog);
+Blog.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+});
+Comment.belongsTo(Blog, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
 
-User.hasMany(Comment);
-Comment.belongsTo(User);
+User.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+});
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
 
 module.exports = {
   User,
