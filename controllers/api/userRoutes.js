e365d72f21b9324e32a7331e9e60915239f2d53b
suzakijun1 <dyong1997@gmail.com>
2023-05-07 14:57:50 -0400

    useroute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
 
 //GET route for specific user
 
-//POST route
+//POST route in order to log in
 router.post("/login", (req, res) => {
   User.findOne({
     where: {
@@ -48,4 +48,14 @@ router.post("/login", (req, res) => {
     });
 });
 
+//POST route to Logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
